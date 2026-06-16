d33293d3df85ea1551110924b862b9a81fb46951
DanielElport <dan.elport@gmail.com>
2026-06-16 20:40:10 +0200

    feat: integrate loading state management in AppRouter and Layout components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from './Header'
+import { useNewsGate } from '../hooks/useNewsGate'
+import LoadingSpinner from './LoadingSpinner'
 
 const Layout = ({ children }: { children: JSX.Element }) => {
+    const { ready, ref } = useNewsGate()
+
     return (
         <div>
             <Header />
+            <div ref={ref} />
             <div className="container white-bg main-container px-sm-4">
-                <div className="pt-2 pt-lg-3">{children}</div>
+                <div className="pt-2 pt-lg-3">
+                    {ready ? children : <LoadingSpinner />}
+                </div>
             </div>
         </div>
     )
